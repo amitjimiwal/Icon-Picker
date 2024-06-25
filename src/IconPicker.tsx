70cc1cc201e98b1605432908c8947878aaf5e4c6
electroo0 <90555965+amitjimiwal@users.noreply.github.com>
2024-06-25 23:52:24 +0530

    add comments

--- src/IconPicker.tsx
+++ src/IconPicker.tsx
@@ -51,15 +51,18 @@ const IconPicker: React.FC<IconPickerProps> = ({
       className="bg-white border border-gray-300 rounded-md shadow-lg mx-auto w-content flex flex-col items-center justify-between"
       style={{ width: `${pickerWidth}px`, height: `${pickerHeight}px` }}
     >
+      {/* header */}
       <h1 className=" bg-gray-300 w-full text-lg font-bold p-3 text-center">
         Select an icon
       </h1>
+      {/* All icons */}
       <div
         className={`grid grid-cols-${columnsInOnePage} gap-3 w-full place-items-center p-4 overflow-y-auto mx-auto`}
         style={{ gridTemplateColumns: `repeat(${columnsInOnePage}, 1fr)` }}
       >
         {renderIcons()}
       </div>
+      {/* Pagination Buttons */}
       <div className="flex justify-between mt-4 gap-2">
         <button
           className="px-4 py-2 bg-blue-500 text-white rounded-md disabled:bg-gray-300"
@@ -81,6 +84,7 @@ const IconPicker: React.FC<IconPickerProps> = ({
           <FaChevronRight />
         </button>
       </div>
+      {/* Footer buttons */}
       <div className=" bg-gray-200 w-full text-lg font-bold p-3 text-center flex justify-end gap-4 items-center">
         <button
           onClick={onClose}
